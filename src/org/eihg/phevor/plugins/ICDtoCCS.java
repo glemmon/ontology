70742a3b47220c95f974483d004cc0cba447180b
Gordon H. Lemmon <glemmon@gmail.com>
2017-10-10 16:34:34 -0600

    use short id instead of level 2

--- src/org/eihg/phevor/plugins/ICDtoCCS.java
+++ src/org/eihg/phevor/plugins/ICDtoCCS.java
@@ -4,22 +4,22 @@ package org.eihg.phevor.plugins;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
 
 import org.apache.commons.collections4.IteratorUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.eihg.phevor.utility.GraphConvenience.Labels;
 import org.eihg.phevor.utility.GraphConvenience.RelTypes;
+import org.neo4j.graphdb.Direction;
 //import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.helpers.collection.FilteringIterator;
 import org.neo4j.server.plugins.Description;
 import org.neo4j.server.plugins.Parameter;
 import org.neo4j.server.plugins.PluginTarget;
@@ -28,15 +28,15 @@ import org.neo4j.server.plugins.Source;
 
 public class ICDtoCCS extends ServerPlugin
 {	
-	private static Logger logger = Logger.getLogger("org.eihg.phevor.plugins"); 
+	//private static Logger logger = Logger.getLogger("org.eihg.phevor.plugins"); 
 
-//	private static boolean has_rel(Node ccs, Node icd10){
-//		for( Relationship r : icd10.getRelationships(Direction.OUTGOING, RelTypes.is_a)){
-//			if( icd10.equals(r.getEndNode() ))
-//				return true;
-//		}
-//		return false;
-//	}
+	private static boolean has_rel(Node ccs, Node icd10){
+		for( Relationship r : icd10.getRelationships(Direction.OUTGOING, RelTypes.is_a)){
+			if( ccs.equals(r.getEndNode() ))
+				return true;
+		}
+		return false;
+	}
 	
 	private static Node lowest(ResourceIterator<Node> node_itr){
 		List<Node> nodes = IteratorUtils.toList(node_itr);
@@ -76,6 +76,15 @@ public class ICDtoCCS extends ServerPlugin
 		return n;
 	}
 	
+	private static Node find_ccs(GraphDatabaseService db, String short_id, String name){
+		Node ccs = db.findNode(Labels.CCSdx, "short_id", short_id);
+		if(ccs == null) return ccs;
+		if(! ccs.hasProperty("name") ){
+				ccs.setProperty("name", name);
+		}
+		return ccs;
+	}
+	
 	private static String get_sans_quotes(Map<String,String> m, String k){
 		return m.get(k).replaceAll("'","").trim();
 	}
@@ -86,37 +95,38 @@ public class ICDtoCCS extends ServerPlugin
 				final Reader reader = new FileReader(in);
 		){
 			final Iterable<CSVRecord> records = format.parse(reader);
-			String last_ccs_long_id = null;
-			Node last_ccs = null;
+			//String last_ccs_long_id = null;
+			//Node last_ccs = null;
+			int i = 0;
+			Transaction tx = db.beginTx();
 			for (CSVRecord record : records) {
-				Transaction tx = db.beginTx();
 				Map<String,String> r_map = record.toMap();
 				String icd10_id = get_sans_quotes(r_map, "'ICD-10-CM CODE'");
 				String icd_name = get_sans_quotes(r_map, "'ICD-10-CM CODE DESCRIPTION'");
 				String ccs_single = get_sans_quotes(r_map, "'CCS CATEGORY'");
-				String ccs_multi = get_sans_quotes(r_map,"'MULTI CCS LVL 2'");
-				if(ccs_multi.isEmpty()) ccs_multi = get_sans_quotes(r_map,"'MULTI CCS LVL 1'");
-				Node ccs;
-				if(ccs_multi.equals(last_ccs_long_id)){
-					ccs = last_ccs;
-				}else{
-					ccs = db.findNode(Labels.CCSdx, "long_id", ccs_multi);
-					last_ccs_long_id = ccs_multi;
-					last_ccs = ccs;
-				}
+				//String ccs_multi = get_sans_quotes(r_map,"'MULTI CCS LVL 2'");
+				String ccs_name = get_sans_quotes(r_map,"'CCS CATEGORY DESCRIPTION'");
+				//if(ccs_single.isEmpty()) ccs_multi = get_sans_quotes(r_map,"'MULTI CCS LVL 1'");
 				Node icd10 = find_icd10(db, icd10_id, icd_name);
-				if(! ccs.hasProperty("short_id") ){
-					try{
-						ccs.setProperty("short_id", Integer.parseInt(ccs_single));
-					}catch(NumberFormatException e){
-						logger.info("short_id: "+ccs_single);
-					}
-				}
+				//Node ccs = ccs_multi.equals(last_ccs_long_id)
+				//		? last_ccs
+				//		: find_ccs(db, ccs_multi, ccs_single, ccs_name);
+				Node ccs = find_ccs(db, ccs_single, ccs_name);
+				//last_ccs_long_id = ccs_multi;
+				//last_ccs = ccs;
 				//if(! has_rel(ccs, icd10)); // For speed we make sure all ICD10-CCS rels are removed
+				if(! has_rel(ccs, icd10));
 				icd10.createRelationshipTo(ccs, RelTypes.is_a);
-				tx.success();
-				tx.close();
+				++i;
+				if(i==99){// batches of 100
+					i=0;
+					tx.success();
+					tx.close();
+					tx = db.beginTx();
+				}
 			}
+			tx.success();
+			tx.close();
 		}
 	}
 	
