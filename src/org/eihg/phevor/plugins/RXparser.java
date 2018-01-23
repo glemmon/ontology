7676c259e9b0bbeea77bf50bd388d5ee021cb52c
Gordon H. Lemmon <glemmon@gmail.com>
2018-01-22 18:32:49 -0700

    int to long

--- src/org/eihg/phevor/plugins/RXparser.java
+++ src/org/eihg/phevor/plugins/RXparser.java
@@ -25,8 +25,13 @@ import org.neo4j.server.plugins.Source;
 public class RXparser extends ServerPlugin
 {	
 	//private static Logger logger = Logger.getLogger("org.eihg.phevor.plugins"); 
+	private static Number get_code(String code_str, int level){
+		return level == 4 ?
+				Long.parseLong(code_str)
+				: Integer.parseInt(code_str);
+	}
 	
-	private static Node create_rx(GraphDatabaseService db, int child_code, String child_name, String type){
+	private static Node create_rx(GraphDatabaseService db, Number child_code, String child_name, String type){
 		Node n = db.createNode(Labels.RX);
 		n.setProperty("id", child_code);
 		n.setProperty("name", child_name);
@@ -42,7 +47,7 @@ public class RXparser extends ServerPlugin
 		String child_code_label = child_name_label+"_CODE";
 		String child_code_str = r.get(child_code_label);
 		if(child_code_str.isEmpty()) return;
-		int child_code = Integer.parseInt(child_code_str);
+		Number child_code = get_code(child_code_str, child_level);
 		String child_name = r.get(child_name_label);
 		Node child = create_rx(db, child_code, child_name, type);
 		int parent_level=child_level-1;
@@ -54,7 +59,7 @@ public class RXparser extends ServerPlugin
 			child.createRelationshipTo(root, RelTypes.is_a);
 			return;
 		}
-		int parent_code = Integer.parseInt(r.get(parent_code_label));
+		Number parent_code = get_code(parent_code_str, parent_level);
 		Node parent = db.findNode(Labels.RX, "id", parent_code);
 		String parent_name = r.get(parent_name_label);
 		if(parent != null){
